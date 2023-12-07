bd17382c73a3b5b5d1e0da7bd8b93ef297f069b1
Jeremy Ward <jeremy@jmichaelward.com>
2023-12-07 11:09:29 -0600

    Prepare for part two by moving invalid game section into different method.

--- src/Assignment/Two/Game.php
+++ src/Assignment/Two/Game.php
@@ -9,10 +9,8 @@ class Game
         public readonly int $red,
         public readonly int $green,
         public readonly int $blue,
-    ) {
-        if ($this->red > 12 || $this->green > 13 || $this->blue > 14) {
-            throw new \InvalidArgumentException('Invalid game.');
-        }
+    )
+    {
     }
 
     public static function createFromData(string $rawData)
@@ -27,9 +25,24 @@ class Game
 
         foreach ($rawValues as $value) {
             [$quantity, $type] = explode(' ', $value);
-            $processed[$type] = (int) $quantity;
+            $processed[$type] = (int)$quantity;
         }
 
         return new Game($processed['red'], $processed['green'], $processed['blue']);
+    }
+
+    public function getRed(): int
+    {
+        return $this->red;
+    }
+
+    public function getGreen(): int
+    {
+        return $this->green;
+    }
+
+    public function getBlue(): int
+    {
+        return $this->blue;
     }
 }
